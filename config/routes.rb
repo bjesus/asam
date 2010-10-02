d93bb1000d1901ee09cdb2a3649096386d52e077
Yo'av Moshe <bjesus@gmail.com>
2010-10-03 01:44:01 +0200

    tag page

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Textmi::Application.routes.draw do
   # first created -> highest priority.
 
   # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
+  match 'tags/:id' => 'texts#tagged'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
