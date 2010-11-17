753973ebe3864021f9ceea37b62c24aefc72746f
Yo'av Moshe <bjesus@gmail.com>
2010-11-18 00:02:32 +0200

    we don't do photos no more

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,18 +1,5 @@
 class Image < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :photo
   belongs_to :user
   belongs_to :text
-
-  after_post_process :hocr
-
-  private
-    def hocr
-      system("hocr -i #{photo.queued_for_write[:original].path} -o /tmp/ocresult")
-      file = File.open("/tmp/ocresult", "rb")
-      contents = file.read
-      photo.instance.text.content = contents
-      photo.instance.text.save()
-      File.delete(file)
-    end
-
 end
