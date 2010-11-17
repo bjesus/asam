c28d141cf47793b5736a666bbac71d7d5d9777b5
Yo'av Moshe <bjesus@gmail.com>
2010-11-18 00:02:22 +0200

    more tags, and relationship

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,5 +1,6 @@
 class Text < ActiveRecord::Base
   versioned
   acts_as_commentable
-  acts_as_taggable_on :year, :language, :author, :tags, :sources
+  acts_as_taggable_on :year, :language, :author, :tags, :sources, :age, :kind, :maagal
+  has_many :images
 end
