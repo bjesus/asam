7b49dbdbdadb00b7feef920e5afcbbfbbcf32d11
bjesus <bjesus@ganuv.(none)>
2010-12-29 09:50:16 +0200

    turn tags to authors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
     Text.tag_counts_on(:author).length
   end
 
+  def to_author(tag)
+    Text.tagged_with(tag, :on => :tags).each do |text|
+      text.tag_list = text.tag_list.remove(tag)
+      text.author_list = text.author_list.add(tag)
+      text.save()
+    end
+  end
+
 end
