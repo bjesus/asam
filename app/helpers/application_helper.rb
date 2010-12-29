2ca1ca703c69ddaf81d49769cd9e6e134179eb39
bjesus <bjesus@ganuv.(none)>
2010-12-29 11:13:45 +0200

    tags managements, relates to...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,11 +8,27 @@ module ApplicationHelper
     Text.tag_counts_on(:author).length
   end
 
+  def related_to(item)
+    same_tags = Text.tagged_with(item.tags)
+    same_tags = same_tags - [item]
+    return same_tags
+  end
+
   def to_author(tag)
     Text.tagged_with(tag, :on => :tags).each do |text|
       text.tag_list = text.tag_list.remove(tag)
       text.author_list = text.author_list.add(tag)
       text.save()
+    end
+  end
+
+  def clear_tag(tag)
+    rm = Text.tagged_with
+    rm.each do |text|
+      text.images.each do |image|
+        image.destroy()
+      end
+     text.destroy()
     end
   end
 
