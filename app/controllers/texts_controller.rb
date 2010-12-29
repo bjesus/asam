77604fbd9b02c5ce58efe0477f81b5518b60ec43
bjesus <bjesus@ganuv.(none)>
2010-12-29 09:25:31 +0200

    show just 50 tags

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -9,7 +9,7 @@ class TextsController < ApplicationController
     @texts = Text.find(:all, :limit => 10, :order=> 'created_at desc')
     @authors = Text.tag_counts_on(:author).limit(10)
     @years = Text.tag_counts_on(:year)
-    @tags = Text.tag_counts_on(:tags).order(:name)
+    @tags = Text.tag_counts_on(:tags).limit(50).order('count desc').sort_by { |t| t.name }
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @texts }
