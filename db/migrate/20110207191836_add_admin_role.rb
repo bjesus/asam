bd0f918946c00cf4b23c10bd087394db2f88f4b6
Yo'av Moshe <bjesus@ganuv.(none)>
2011-02-07 21:25:01 +0200

    admin role

---
+++ db/migrate/20110207191836_add_admin_role.rb
@@ -0,0 +1,10 @@
+class AddAdminRole < ActiveRecord::Migration
+  def self.up
+    add_column :users, :admin, :boolean, :default => false
+  end
+
+  def self.down
+    remove_column :users, :admin
+  end
+end
+
