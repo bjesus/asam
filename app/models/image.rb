985a526589cf84606749edf69eadb1d0b4b24dd7
Yo'av Moshe <bjesus@gmail.com>
2011-09-28 17:28:17 +0300

    don't fail when can't thumbnail

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,10 +1,7 @@
 # encoding: utf-8
 
 class Image < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :thumb => { :geometry => "85x120>",
-                        :quality => 60,
-                        :format => 'JPG' }}
-                    #:processors => [:thumbnail, :unoconv]
+  has_attached_file :photo, :styles => { :thumb => { :geometry => "85x120>", :quality => 60, :format => 'JPG' } }, :whiny => false
   belongs_to :user
   belongs_to :text
 end
