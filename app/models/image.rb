69deaeda822c704eea119be8c5388635edd14caf
Yo'av Moshe <bjesus@gmail.com>
2011-09-28 17:42:07 +0300

    this solves utf8 problems for some reason

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,10 @@
 # encoding: utf-8
 
 class Image < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :thumb => { :geometry => "85x120>", :quality => 60, :format => 'JPG' } }, :whiny => false
+  has_attached_file :photo,
+                        :styles => { :thumb => { :geometry => "85x120>", :quality => 60, :format => 'JPG' } },
+                        :whiny => false, 
+                        :path => ":rails_root/public/system/:attachment/:id/:style/:filename"
   belongs_to :user
   belongs_to :text
 end
