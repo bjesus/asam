597fa3ded0a19f0c77bed5f0f595ecab3edefa32
Yo'av Moshe <bjesus@localhost.localdomain>
2012-12-26 01:42:16 +0200

    extract word content

--- app/models/image.rb
+++ app/models/image.rb
@@ -12,4 +12,24 @@ class Image < ActiveRecord::Base
 
   ajaxful_rateable :stars => 5, :dimensions => [:quality], :allow_update => true, :cache_column => :rating_average
 
+
+  before_save :extract_data
+
+
+  def msword?
+    photo_content_type =~ %r{^application/(?:ms-word|msword|wordprocessingml)$}
+  end
+
+  private
+
+  # Retrieves metadata for Word files
+  def extract_data
+    return unless msword?
+    path = photo.queued_for_write[:original].path
+    require 'open3'
+    data = ""
+    Open3.popen3("antiword", path) { |i, o| data = o.read }
+    self.description = data
+  end
+
 end
