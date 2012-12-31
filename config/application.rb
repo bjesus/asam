baa23f2dc4fc956d79534cfad1a3acc95fd8c722
Yo'av Moshe <bjesus@localhost.localdomain>
2012-12-31 21:46:23 +0200

    I18n: english interface (WIP)

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Textmi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
---
+++ config/initializers/shelves.rb
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+require "ostruct"
+     
+App = OpenStruct.new({
+  :title => "מעין",
+  :organization => OpenStruct.new({
+	  :name => "דרור ישראל",
+	  :url => "http://www.drorisrael.org.il"
+  })
+})
